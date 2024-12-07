80c31272bcf6e683c0326c170b42713dff42eca3
Blake Arnold <barnold@blakearnold.me>
2024-12-07 09:46:18 -0500

    basic loop structure

--- day5/main.ts
+++ day5/main.ts
@@ -2,12 +2,28 @@ import { readFile } from "../lib/readFile.ts";
 
 const data = await readFile("input").then(data => data.split("\n\n")); // read the file and split it on the blank line
 
-const rules: Map<number, number[]> = new Map();
+const productionRules: Map<number, number[]> = new Map();
 data[0].split("\n").map(row => row.split("|").map(Number))
     .forEach(([key, value]) => {
-        if (rules.has(key)) {
-            rules.get(key)?.push(value);
+        if (productionRules.has(key)) {
+            productionRules.get(key)?.push(value);
         } else {
-            rules.set(key, [value]);
+            productionRules.set(key, [value]);
         }
     });
+
+const productionQueue = data[1].split("\n").map(row => row.split(",").map(Number));
+
+for (const pages of productionQueue) {
+    for (const page of pages) {
+        if (productionRules.has(page)) {
+            const rules = productionRules.get(page);
+            if (rules) {
+                for (const rule of rules) {
+                    if (!pages.includes(rule)) break;
+                }
+            }
+        }
+    }
+}
+
